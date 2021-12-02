19b1901399cd95fb4e7c1b82c1aa8f99fc7a3315
jdelarubia <jdelarubia@gmail.com>
2021-12-02 20:51:53 +0100

    add new sample code

--- app.ts
+++ app.ts
@@ -1,7 +1,9 @@
 import { sampleWeb } from "./web-platform-api/web.ts";
 import { launchServer } from "./http-server-api/server.ts";
 import { samplePermissions } from "./permission-api/permission.ts";
+import { sampleStorage } from "./web-storage-api/storage.ts";
 
 await samplePermissions();
 await sampleWeb();
 await launchServer();
+await sampleStorage();
