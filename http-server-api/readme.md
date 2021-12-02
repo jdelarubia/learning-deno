1253aa1ccaf60f2450f112ac1f508e3baa12344a
jdelarubia <jdelarubia@gmail.com>
2021-12-02 19:59:37 +0100

    add http-server-api

---
+++ http-server-api/readme.md
@@ -0,0 +1,12 @@
+# HTTP Server APIs
+
+Listen on `127.0.0.1:8080` for `GET` requests.
+
+Prompts the user for access to the net. If user confirms, starts the server;
+otherwise the program ends.
+
+> NOTE: a `POST` request will throw an error.
+
+## Resources
+
+- [Deno's official documentation](https://deno.land/manual@v1.16.3/runtime/http_server_apis)
---
+++ http-server-api/server.ts
@@ -0,0 +1,50 @@
+import { requestPermission } from "../permission-api/permission.ts";
+
+// Settings
+const PORT = 8080;
+const HOST = "127.0.0.1";
+
+async function requestNetAccess(): Promise<boolean> {
+  const descriptor: Deno.PermissionDescriptor = { name: "net" } as const;
+  const status = await requestPermission(descriptor);
+  return status.state === "granted";
+} //.
+
+// Info about HTTP Requests and Responses
+// https://deno.land/manual@v1.16.3/runtime/http_server_apis
+async function launchServer() {
+  console.log(
+    `If you grant NET permissions, the server will listen on http://${HOST}:${PORT}`,
+  );
+  if (await requestNetAccess()) {
+    const options: Deno.ListenOptions = { port: PORT, hostname: HOST };
+    const server: Deno.Listener = await Deno.listen(options);
+
+    for await (const conn of server) {
+      handle(conn);
+    }
+  }
+} //.
+
+/**
+ * HELPER
+ * * NOTE This method throws an Exception if method is other than GET.
+ * @param conn : Deno.Conn
+ */
+async function handle(conn: Deno.Conn) {
+  const httpConnection: Deno.HttpConn = Deno.serveHttp(conn);
+
+  for await (const requestEvent of httpConnection) {
+    console.log(requestEvent);
+    const { method, redirect, url } = requestEvent.request;
+    console.log(method, redirect, url);
+    const path = new URL(requestEvent.request.url);
+    const strResponse = `request from url: ${path.pathname}`;
+    const response = new Response(strResponse, { status: 200 });
+    console.log(strResponse);
+
+    await requestEvent.respondWith(response);
+  }
+} //.
+
+export { launchServer };
