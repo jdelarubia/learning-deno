cdcd0a2378bc8d0783b0915eaab10d5cdbb65c83
jdelarubia <jdelarubia@gmail.com>
2021-12-02 20:51:21 +0100

    add web-storage-api

---
+++ web-storage-api/readme.md
@@ -0,0 +1,8 @@
+# Web Storage API
+
+Tests the `localStorage` functionality. Similar to the browser-based version.
+Very handy!
+
+## Resources
+
+- [Deno's official documentation](https://deno.land/manual@v1.16.3/runtime/web_storage_api)
---
+++ web-storage-api/storage.ts
@@ -0,0 +1,50 @@
+import { heroRepository as repo } from "./heroRepository.ts";
+
+function initDB() {
+  console.log("Initializing our repository");
+  repo.add({ name: "Godzilla", powers: "Super Strength, Endurance" });
+  repo.add({
+    name: "Death",
+    powers: "Accelerated Healing, Astral Projection",
+  });
+  repo.add({ name: "Elektra", powers: "Acrobatics, Endurance" });
+  repo.add({ name: "Captain Marvel", powers: "" });
+  repo.add({ name: "Aegis", powers: "Accelerated Healing, Clairvoyance" });
+  repo.add({ name: "Batman", powers: "Acrobatics, Enhanced Memory" });
+  repo.add({ name: "couch potato", powers: "watch tv, order fastfood" });
+} //.
+
+function printLength() {
+  console.log(`Our repository currently has ${repo.length()} elements.`);
+} //.
+
+function printAll() {
+  console.log("All our heroes/villains");
+  const all = repo.getAll();
+  for (const hero in all) {
+    console.log(all[hero]);
+  }
+} //.
+
+function clearAll() {
+  console.log("Clearing our our repository");
+  repo.clear();
+} //.
+
+function removeCouchPotato() {
+  console.log(
+    "We detected an outlier. Removing Mr.'Couch Potato' from our repo",
+  );
+  repo.remove("couch potato");
+} //.
+
+function sampleStorage() {
+  initDB();
+  printLength();
+  printAll();
+  removeCouchPotato();
+  printLength();
+  clearAll();
+} //.
+
+export { sampleStorage };
