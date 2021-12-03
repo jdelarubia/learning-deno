69ac2eb88fdf3ff6b5b75cab3e81bbd60c32e3b2
jdelarubia <jdelarubia@gmail.com>
2021-12-03 23:08:45 +0100

    update our sample code

--- app.ts
+++ app.ts
@@ -2,8 +2,10 @@ import { sampleWeb } from "./web-platform-api/web.ts";
 import { launchServer } from "./http-server-api/server.ts";
 import { samplePermissions } from "./permission-api/permission.ts";
 import { sampleStorage } from "./web-storage-api/storage.ts";
+import { sampleWorkers } from "./web-worker-api/workers.ts";
 
 await samplePermissions();
 await sampleWeb();
 await launchServer();
 await sampleStorage();
+sampleWorkers();
