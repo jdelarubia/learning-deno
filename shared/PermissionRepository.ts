ccc6c8d62003771e60e51518ede88b8660ef7d57
jdelarubia <jdelarubia@gmail.com>
2021-12-19 22:22:21 +0100

    Class to manage app permissions centralized

---
+++ shared/PermissionRepository.ts
@@ -0,0 +1,64 @@
+/**
+ * PermissionHandler.ts
+ * Handles regular Deno permissions (except unstable) of our application.
+ *
+ * Example:
+ * const readPermission: Deno.PermissionDescriptor = { name: "read", path: "./worker.ts" };
+ * const permissionHandler = new PermissionHandler(readPermission);
+ * if (await permissionHandler.isGranted()) {
+ * // do something
+ * }
+ *
+ */
+
+type _Descriptor = Deno.PermissionDescriptor;
+type _Status = Deno.PermissionStatus;
+
+class PermissionRepository {
+  private descriptor: _Descriptor | null = null;
+
+  /**
+   * Setup a PermissionDescriptor.
+   * @param descriptor: Deno.PermissionDescriptor
+   */
+  constructor() {
+    // this.descriptor = descriptor as _Descriptor;
+  } //.
+
+  /**
+   * Query permissions set in this.descriptor.
+   * @returns Promise<Deno.PermissionStatus>
+   */
+  async query(descriptor: _Descriptor): Promise<_Status> {
+    return await Deno.permissions.query(descriptor as _Descriptor);
+  } //.
+
+  /**
+   * Request if this.descriptor is enabled.
+   * @returns Promise<Deno.PermissionStatus>
+   */
+  async request(descriptor: _Descriptor): Promise<_Status> {
+    return await Deno.permissions.request(descriptor as _Descriptor);
+  } //.
+
+  /**
+   * Revoke the set of permissions in this.descriptor.
+   * @returns Promise<Deno.PermissionStatus>
+   */
+  async revoke(descriptor: _Descriptor): Promise<_Status> {
+    return await Deno.permissions.revoke(descriptor as _Descriptor);
+  } //.
+
+  /**
+   * Return true if user has granted this.descriptor.
+   * @returns Promise<boolean>
+   */
+  async isGranted(descriptor: _Descriptor): Promise<boolean> {
+    return (await (await this.request(descriptor)).state) === "granted";
+  } //.
+} //. PermissionHandler
+
+const permissionRepo = new PermissionRepository();
+
+export { permissionRepo };
+export type { _Descriptor, _Status };
