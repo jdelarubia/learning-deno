32eabef1a3f9e8978e862613f08e87277764208f
jdelarubia <jdelarubia@gmail.com>
2021-12-19 22:23:02 +0100

    make use of permissions repository

--- permission-api/permission.ts
+++ permission-api/permission.ts
@@ -1,4 +1,6 @@
-const descriptors: { [index: string]: Deno.PermissionDescriptor } = {
+import { permissionRepo, _Descriptor } from "../shared/PermissionRepository.ts";
+
+const descriptors: { [index: string]: _Descriptor } = {
   read: { name: "read" },
   write: { name: "write" },
   net: { name: "net" },
@@ -9,25 +11,16 @@ const descriptors: { [index: string]: Deno.PermissionDescriptor } = {
 };
 
 /**
- * HELPER
- * @param permissionDescriptor Deno.PermissionDescriptor
- * @returns Promise<Deno.PermissionStatus>
- */
-async function checkCurrentPermission(
-  permissionDescriptor: Deno.PermissionDescriptor,
-): Promise<Deno.PermissionStatus> {
-  return await Deno.permissions.query(permissionDescriptor);
-} //.
-
-/**
  * Console log all current permission of our app.
  */
 async function logCurrentPermissions() {
-  console.log("*** logging current permissions ***");
+  console.log("*** log all current system permissions ***");
 
   for (const descriptor in descriptors) {
-    const query = await checkCurrentPermission(descriptors[descriptor]);
-    console.log(`${descriptor}: `, query);
+    console.log(
+      `${descriptor}: `,
+      await permissionRepo.query(descriptors[descriptor])
+    );
   }
   console.log();
 } //.
@@ -41,45 +34,38 @@ async function sampleRequestPermission() {
     name: "write",
     path: "./output.txt",
   } as const;
-  const before = await checkCurrentPermission(descriptor);
+
+  const before = await permissionRepo.query(descriptor);
   console.log("permissions before prompting the user:", before);
-  const status = await Deno.permissions.request(descriptor);
-  console.log("permissions AFTER prompting the user:", status);
+  const after = await permissionRepo.request(descriptor);
+  console.log("permissions AFTER prompting the user:", after);
   console.log();
 } //.
 
 /**
  * Test revoking permissions by prompting the user
  */
-async function revokePermission() {
+async function sampleRevokePermission() {
   console.log("*** Revoking a permission ***");
   console.log(
-    "Try running this program with the flag --allow-net enabled to see it revoked",
+    "Try running this program with the flag --allow-net enabled to see it revoked"
   );
   const descriptor = { name: "net", host: "127.0.0.1" } as const;
-  const before = await checkCurrentPermission(descriptor);
-  console.log("permissions before prompting the user:", before);
-  const status = await Deno.permissions.revoke(descriptor);
-  console.log("permissions AFTER prompting the user:", status);
-  console.log();
-} //.
 
-/**
- * Given a permission descriptor, prompts the user if he/she wants to grant access.
- * @param descriptor : Deno.Permissiondescriptor
- * @returns Promise<Deno.PermissionStatus>
- */
-async function requestPermission(descriptor: Deno.PermissionDescriptor) {
-  return await Deno.permissions.request(descriptor);
+  const before = await permissionRepo.query(descriptor);
+  console.log("permissions before prompting the user:", before);
+  const after = await permissionRepo.revoke(descriptor);
+  console.log("permissions AFTER prompting the user:", after);
+  console.log();
 } //.
 
 /**
  * Main functionality to run all our sample code.
  */
-async function samplePermissions() {
+async function run() {
   await logCurrentPermissions();
   await sampleRequestPermission();
-  await revokePermission();
+  await sampleRevokePermission();
 } //.
 
-export { requestPermission, samplePermissions };
+export { run as demoPermissions };
