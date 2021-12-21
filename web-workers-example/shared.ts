310c528b026f20915fcbb171c648f88345b3fe03
jdelarubia <jdelarubia@gmail.com>
2021-12-21 20:49:46 +0100

    types and functions shared in the demo

---
+++ web-workers-example/shared.ts
@@ -0,0 +1,19 @@
+/**
+ * shared.ts
+ * Custom types and utility functions.
+ */
+
+export type _DenoWorkerOptions = { deno?: { [index: string]: boolean } };
+export type _CommandReturn = (args: string) => boolean;
+export type _RepoIndex = string | number;
+export type _Specs = { [feature: string]: string | number };
+export type _Product = {
+  id?: number;
+  specs: _Specs;
+};
+
+const isBoolean = (val: string | boolean): boolean => {
+  return typeof val === "boolean";
+}; //.
+
+export { isBoolean };
