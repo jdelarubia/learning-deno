c7eb9bd32ed7dcb77d6a05807139bedf3cc914d8
jdelarubia <jdelarubia@gmail.com>
2021-12-21 20:52:43 +0100

    add new demos to our main app

--- app.ts
+++ app.ts
@@ -4,7 +4,7 @@
  */
 
 import { permissionRepo } from "./shared/PermissionRepository.ts";
-import { demoPermissions } from "./permission-api/permission.ts";
+import { demoPermissions } from "./permission-api/app.ts";
 import { demoWebPlatform } from "./web-platform-api/app.ts";
 import { demoHttpServer } from "./http-server-api/app.ts";
 import { demoStorage } from "./web-storage-api/app.ts";
@@ -20,7 +20,10 @@ async function askPermissions() {
     { name: "write", path: "./output.txt" },
     { name: "net", host: "127.0.0.1:8080" },
     { name: "net", host: "localhost:8080" },
+    { name: "read", path: "./web-workers-example/logger.ts" },
+    { name: "write", path: "./web-workers-example/session.txt" },
   ];
+  console.log("Please, enable permissions so the demo app can work");
   await permissionDescriptors.forEach((descriptor) => {
     permissionRepo.request(descriptor);
   });
@@ -33,5 +36,4 @@ await demoWebPlatform();
 // await demoHttpServer(); // blocks the execution running a web server
 await demoStorage();
 await demoWorkers();
-
-// await demoSampleWorkers(); // ongoing
+await demoSampleWorkers();
