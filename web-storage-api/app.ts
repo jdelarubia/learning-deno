acac485b9d9e64b8ea3133b112977ca902aed217
jdelarubia <jdelarubia@gmail.com>
2021-12-23 22:35:18 +0100

    extract fake data to a mocks file

--- web-storage-api/app.ts
+++ web-storage-api/app.ts
@@ -4,28 +4,20 @@
  */
 
 import { heroRepository as repo } from "./heroRepository.ts";
+import { fakeDB } from "./mocks.ts";
 
 function initDB() {
   console.log("Initializing our repository");
-  repo.add({ name: "Godzilla", powers: "Super Strength, Endurance" });
-  repo.add({
-    name: "Death",
-    powers: "Accelerated Healing, Astral Projection",
-  });
-  repo.add({ name: "Elektra", powers: "Acrobatics, Endurance" });
-  repo.add({ name: "Captain Marvel", powers: "" });
-  repo.add({ name: "Aegis", powers: "Accelerated Healing, Clairvoyance" });
-  repo.add({ name: "Batman", powers: "Acrobatics, Enhanced Memory" });
-  repo.add({ name: "couch potato", powers: "watch tv, order fastfood" });
+  fakeDB.forEach((hero) => repo.add(hero));
 } //.
 
 function printLength() {
-  console.log(`Our repository currently has ${repo.length()} elements.`);
+  console.log(`Our repository currently has ${repo.len()} elements.`);
 } //.
 
 function printAll() {
   console.log("All our heroes/villains");
-  const all = repo.getAll();
+  const all = repo.findAll();
   for (const hero in all) {
     console.log(all[hero]);
   }
---
+++ web-storage-api/mocks.ts
@@ -0,0 +1,12 @@
+export const fakeDB = [
+  { name: "Godzilla", powers: "Super Strength, Endurance" },
+  {
+    name: "Death",
+    powers: "Accelerated Healing, Astral Projection",
+  },
+  { name: "Elektra", powers: "Acrobatics, Endurance" },
+  { name: "Captain Marvel", powers: "" },
+  { name: "Aegis", powers: "Accelerated Healing, Clairvoyance" },
+  { name: "Batman", powers: "Acrobatics, Enhanced Memory" },
+  { name: "couch potato", powers: "watch tv, order fastfood" },
+];
