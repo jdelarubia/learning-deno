6498872205d577e768c8fe1b7432c8cf68a1610c
jdelarubia <jdelarubia@gmail.com>
2021-12-24 18:37:27 +0100

    deno auto-formatting

--- tests/web-storage-api/repo.test.ts
+++ tests/web-storage-api/repo.test.ts
@@ -27,7 +27,7 @@ Deno.test({
     assertNotEquals(
       heroRepository.len(),
       0,
-      "len() must return a value greater than 0 after init"
+      "len() must return a value greater than 0 after init",
     );
   },
 });
@@ -45,7 +45,7 @@ Deno.test({
   fn: () => {
     assert(
       heroRepository.findAll().length > 1,
-      "repository doesn't seem to be initialized"
+      "repository doesn't seem to be initialized",
     );
   },
 });
@@ -60,7 +60,7 @@ Deno.test({
     assertNotEquals(
       heroRepository.len(),
       0,
-      "len() must return a value greater than 0 after init"
+      "len() must return a value greater than 0 after init",
     );
   },
 });
