cf9583aaa407eb9cad4735309d72249adaf08a47
jdelarubia <jdelarubia@gmail.com>
2021-12-24 17:58:38 +0100

    auto-formatting

--- tests/permissions-api/repo.test.ts
+++ tests/permissions-api/repo.test.ts
@@ -17,25 +17,27 @@ const readPermissionDescriptor: Deno.PermissionDescriptor = {
 };
 
 Deno.test({
-  name: "permissionrepo. query() on a non --allow-read flagged file return prompt",
+  name:
+    "permissionrepo. query() on a non --allow-read flagged file return prompt",
   fn: async () => {
     const p = await permissionRepo.query(readPermissionDescriptor);
     assertEquals(
       p.state,
       "prompt",
-      "query() non --allow-read flagged file should return prompt by default"
+      "query() non --allow-read flagged file should return prompt by default",
     );
   },
 });
 
 Deno.test({
-  name: "permissionrepo. isGranted() on a non --allow-read flagged file return false",
+  name:
+    "permissionrepo. isGranted() on a non --allow-read flagged file return false",
   fn: async () => {
     const p: boolean = await permissionRepo.isGranted(readPermissionDescriptor);
     assertEquals(
       p,
       false,
-      "isGranted() o a non --allow-read flagged file should return false"
+      "isGranted() o a non --allow-read flagged file should return false",
     );
   },
 });
