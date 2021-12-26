3cb4e2f1bd19fa99290c15ccc0d67e1e71378cd7
jdelarubia <jdelarubia@gmail.com>
2021-12-26 23:16:27 +0100

    read demo to execute from command line

--- app.ts
+++ app.ts
@@ -9,9 +9,11 @@ import { demoWebPlatform } from "./web-platform-api/app.ts";
 import { demoHttpServer } from "./http-server-api/app.ts";
 import { demoStorage } from "./web-storage-api/app.ts";
 import { demoWorkers } from "./web-worker-api/app.ts";
-import { demoSampleWorkers } from "./web-workers-example/app.ts";
+import { demoSampleWorkers } from "./ex-web-workers/app.ts";
 
-// Request all permissions needed to run our app
+/**
+ * Request all permissions needed to run our app.
+ */
 async function askPermissions() {
   const permissionDescriptors: Deno.PermissionDescriptor[] = [
     { name: "read", path: "./web-platform-api/albums.json" },
@@ -20,8 +22,8 @@ async function askPermissions() {
     { name: "write", path: "./output.txt" },
     { name: "net", host: "127.0.0.1:8080" },
     { name: "net", host: "localhost:8080" },
-    { name: "read", path: "./web-workers-example/logger.ts" },
-    { name: "write", path: "./web-workers-example/session.txt" },
+    { name: "read", path: "./ex-web-workers/logger.ts" },
+    { name: "write", path: "./ex-web-workers/session.txt" },
   ];
   console.log("Please, enable permissions so the demo app can work");
   await permissionDescriptors.forEach((descriptor) => {
@@ -29,11 +31,44 @@ async function askPermissions() {
   });
 } //.
 
-// Run all the demos
-await askPermissions();
-await demoPermissions();
-await demoWebPlatform();
-// await demoHttpServer(); // blocks the execution running a web server
-await demoStorage();
-await demoWorkers();
-await demoSampleWorkers();
+type _MenuElements = {
+  [index: string]: { fn: CallableFunction; desc?: string };
+};
+const supportedFunctions: _MenuElements = {
+  permissions: { fn: demoPermissions, desc: "Demo Deno Permissions API" },
+  web: { fn: demoWebPlatform, desc: "Demo Deno Web Platform API" },
+  http: {
+    fn: demoHttpServer,
+    desc: "Demo HTTP server API (blocks the process)",
+  },
+  storage: { fn: demoStorage, desc: "Demo Deno Local Storage API" },
+  workers: { fn: demoWorkers, desc: "Demo Deno Worker API" },
+  exworker: {
+    fn: demoSampleWorkers,
+    desc: "Demo app of Worker + Local Storage + Permissions",
+  },
+};
+
+/**
+ * Main app.
+ * Check for arguments.
+ * Show a list of demos and examples available, AND
+ * run whichever functions we pass on via command-line arguments.
+ */
+async function main() {
+  if (Object.keys(Deno.args).length > 0) {
+    await askPermissions();
+    console.log("Demos available:");
+    for (const fun in supportedFunctions) {
+      const { desc } = supportedFunctions[fun];
+      console.log(fun, ":", desc);
+    }
+
+    for (const fun of Deno.args) {
+      const { fn } = supportedFunctions[fun];
+      await fn();
+    }
+  }
+} //.
+
+main();
