74c1639461dbdc0276549bb681d7a791be423272
jdelarubia <jdelarubia@gmail.com>
2021-12-27 22:49:31 +0100

    add a new example

---
+++ ex-unix-cat/app.ts
@@ -0,0 +1,23 @@
+/**
+ * app.ts
+ * Implementation of unix's cat utility.
+ */
+
+import { copy } from "https://deno.land/std@0.119.0/streams/conversion.ts";
+
+async function run() {
+  for (const filename of Deno.args) {
+    try {
+      const file = await Deno.open(filename);
+      await copy(file, Deno.stdout);
+      file.close();
+    } catch (error) {
+      if (error instanceof Deno.errors.NotFound) {
+        console.log(`${error.name}: ${error.message}`);
+      }
+      Deno.exit();
+    }
+  }
+} //.
+
+export { run as demoUnixCat };
---
+++ ex-unix-cat/readme.md
@@ -0,0 +1,12 @@
+# Unix cat utility with deno
+
+Stand-alone example utility as shown on
+[Deno's official website](https://deno.land/manual@main/examples/unix_cat).
+
+## Running the example
+
+From the directory the example is on, type:
+
+```bash
+$ deno run --allow-read ./app.ts passwords.txt devices.txt
+```
