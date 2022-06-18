ab1af1ce0b4fad803eb2709b8b5fc2257403492b
jdelarubia <jdelarubia@gmail.com>
2022-06-18 13:51:38 +0200

    hardcode sample data

---
+++ ex-crud-api/controllers/data.ts
@@ -0,0 +1,24 @@
+import { Product } from '../types.ts';
+
+const products: Product[] = [
+  {
+    id: '1',
+    name: 'Product One',
+    description: 'Amazing product one!',
+    price: 29.99,
+  },
+  {
+    id: '2',
+    name: 'Product Two',
+    description: 'Product Two',
+    price: 49.99,
+  },
+  {
+    id: '3',
+    name: 'Product Three',
+    description: 'Product Three of our catalogue',
+    price: 59.99,
+  },
+];
+
+export { products };
