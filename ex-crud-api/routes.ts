0f8abb498484548f4717aa5ab2930d14fec294ea
jdelarubia <jdelarubia@gmail.com>
2022-06-18 13:53:07 +0200

    document module

--- ex-crud-api/routes.ts
+++ ex-crud-api/routes.ts
@@ -1,7 +1,13 @@
+/**
+ * routes.ts
+ * Define all the endpoints of our API.
+ */
+
 import { Router, Context } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 import { findAll } from './controllers/products.ts';
 
 const router = new Router();
+
 router.get('/api/v1/products', findAll);
 
 export default router;
