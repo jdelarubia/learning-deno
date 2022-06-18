5e222342ac0c8223ce426b89c30d528b2bc8a883
jdelarubia <jdelarubia@gmail.com>
2022-06-18 13:52:12 +0200

    products controller

---
+++ ex-crud-api/controllers/products.ts
@@ -0,0 +1,21 @@
+/**
+ * products.ts
+ * Module that interface with our products.
+ */
+
+import { Context } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
+
+import { products } from './data.ts';
+
+/**
+ * @desc  Get all products
+ * @route GET /api/v1/products
+ */
+const findAll = (context: Context) => {
+  context.response.body = {
+    success: true,
+    data: products,
+  };
+};
+
+export { findAll };
