5bd282b76dea3f69972dc48aceb8d3962ffc6bd4
jdelarubia <jdelarubia@gmail.com>
2022-06-18 13:52:54 +0200

    bind our products controller

--- ex-crud-api/routes.ts
+++ ex-crud-api/routes.ts
@@ -1,8 +1,7 @@
 import { Router, Context } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
+import { findAll } from './controllers/products.ts';
 
 const router = new Router();
-router.get('/api/v1/products', (context: Context) => {
-  context.response.body = 'Hello World';
-});
+router.get('/api/v1/products', findAll);
 
 export default router;
