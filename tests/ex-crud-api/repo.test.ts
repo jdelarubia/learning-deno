5258d6243446a379563ede91ccde9292afccf2f4
jdelarubia <jdelarubia@gmail.com>
2022-06-20 21:45:42 +0200

    use changed repository

--- tests/ex-crud-api/repo.test.ts
+++ tests/ex-crud-api/repo.test.ts
@@ -9,27 +9,27 @@ import {
   assertExists,
   assertNotEquals,
 } from '../deps.ts';
-import { fakeDB } from '../../ex-crud-api/controllers/db.ts';
+import { productsRepo } from '../../ex-crud-api/controllers/db.ts';
 import { OptionalProduct } from '../../ex-crud-api/types.ts';
 
 Deno.test({
   name: 'productRepo. accessing a non existing product return undefined',
   fn: () => {
-    assertEquals(fakeDB.one('100'), undefined);
+    assertEquals(productsRepo.find('100'), undefined);
   },
 });
 
 Deno.test({
   name: 'productRepo. len() of initial DB length is 3',
   fn: () => {
-    assertEquals(fakeDB.len(), 3, 'Initial len() must be 3 elements');
+    assertEquals(productsRepo.len(), 3, 'Initial len() must be 3 elements');
   },
 });
 
 Deno.test({
   name: 'productRepo. all() return an array of Products and initial names match',
   fn: () => {
-    const allProducts = fakeDB.all();
+    const allProducts = productsRepo.findAll();
     assertEquals(allProducts.length, 3);
     assertEquals(allProducts[0].name, 'Product One');
     assertEquals(allProducts[1].name, 'Product Two');
@@ -41,7 +41,7 @@ Deno.test({
   name: 'productRepo. one(2).name() must be "Product Two"',
   fn: () => {
     assertEquals(
-      fakeDB.one('2')?.name,
+      productsRepo.find('2')?.name,
       'Product Two',
       "2nd product in DB doesn't seem right"
     );
@@ -56,8 +56,33 @@ Deno.test({
       description: 'Fourth product in our catalogue',
       price: 75.55,
     };
-    const newId = fakeDB.add(exampleProduct);
-    assertEquals(fakeDB.len(), 4);
-    assertEquals(fakeDB.one(newId)?.name, 'Product Four');
+    const newId = productsRepo.add(exampleProduct);
+    assertEquals(productsRepo.len(), 4);
+    assertEquals(productsRepo.find(newId)?.name, 'Product Four');
+  },
+});
+
+Deno.test({
+  name: 'productRepo. update() change the name of product',
+  fn: () => {
+    const initialName: string | undefined = productsRepo.find('2')?.name;
+    const exampleProduct: OptionalProduct = {
+      name: 'Updated Product TWO',
+      description: 'Fourth product in our catalogue',
+      price: 75.55,
+    };
+    productsRepo.update(exampleProduct);
+    const updatedName: string | undefined = productsRepo.find('2')?.name;
+    console.log('names:', initialName, updatedName);
+    assertEquals(productsRepo.len(), 4);
+    assertNotEquals(initialName, updatedName);
+  },
+});
+
+Deno.test({
+  name: 'productRepo. remove() change DB length',
+  fn: () => {
+    productsRepo.remove('2');
+    assertEquals(productsRepo.len(), 3);
   },
 });
