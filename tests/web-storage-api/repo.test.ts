241f6cf74b64e6583a8ae1c34e6e739c129437ff
jdelarubia <jdelarubia@gmail.com>
2022-06-20 21:46:01 +0200

    tweak test messages

--- tests/web-storage-api/repo.test.ts
+++ tests/web-storage-api/repo.test.ts
@@ -27,7 +27,7 @@ Deno.test({
     assertNotEquals(
       heroRepository.len(),
       0,
-      'len() must return a value greater than 0 after init',
+      'len() must return a value greater than 0 after init'
     );
   },
 });
@@ -45,7 +45,7 @@ Deno.test({
   fn: () => {
     assert(
       heroRepository.findAll().length > 1,
-      'repository doesn\'t seem to be initialized',
+      "repository doesn't seem to be initialized"
     );
   },
 });
@@ -60,7 +60,7 @@ Deno.test({
     assertNotEquals(
       heroRepository.len(),
       0,
-      'len() must return a value greater than 0 after init',
+      'len() must return a value greater than 0 after init'
     );
   },
 });
@@ -71,7 +71,7 @@ Deno.test({
     const initLen: number = heroRepository.len();
     heroRepository.remove('Godzilla');
     const finalLen: number = heroRepository.len();
-    assert(finalLen < initLen, 'remove() doesn\'t seem to subtract repo len');
+    assert(finalLen < initLen, "remove() doesn't seem to subtract repo len");
   },
 });
 
