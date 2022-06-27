a887c4cd046fd1d50d51389473073a92d7db9695
jdelarubia <jdelarubia@gmail.com>
2022-06-27 22:27:22 +0200

    option to read port from environment variable

--- ex-crud-api/app.ts
+++ ex-crud-api/app.ts
@@ -2,14 +2,14 @@ import { Application } from 'https://deno.land/x/oak@v10.6.0/mod.ts';
 import productsRouter from './routes.ts';
 
 const HOST = '127.0.0.1';
-const PORT = 8080;
+const PORT = Deno.env.get('PORT') || 8080;
 
 const run = async () => {
   const app = new Application();
   app.use(productsRouter.routes());
   app.use(productsRouter.allowedMethods());
   console.log(`Server running on ${HOST}:${PORT}`);
-  await app.listen({ hostname: HOST, port: PORT });
+  await app.listen({ hostname: HOST, port: +PORT });
 };
 
 export { run as demoCrudApi };
