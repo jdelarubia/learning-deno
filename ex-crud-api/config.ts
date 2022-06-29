f61b5ebeba3f73f51a94849b2d82bf2b7ed99f1d
jdelarubia <jdelarubia@gmail.com>
2022-06-29 22:06:11 +0200

    attempt to read db config from env
    give some defaults for db credentials but try to read them
    from environment first.

--- ex-crud-api/config.ts
+++ ex-crud-api/config.ts
@@ -1,9 +1,15 @@
+const __DB__ = 'products';
+const __DBUSER__ = Deno.env.get('DBUSER') || 'root';
+const __DBPASSWORD__ = Deno.env.get('DBPASSWORD') || 'root';
+const __DBHOST__ = Deno.env.get('DBHOSTNAME') || '127.0.0.1';
+const __DBPORT__ = Deno.env.get('DBPORT') || 5432;
+
 const dbCreds = {
-  user: 'root',
-  database: 'products',
-  password: 'root',
-  hostname: '127.0.0.1',
-  port: 5432,
+  user: __DBUSER__,
+  database: __DB__,
+  password: __DBPASSWORD__,
+  hostname: __DBHOST__,
+  port: __DBPORT__,
 };
 
 export { dbCreds };
